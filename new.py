c4734ec2ef619e6e1f2d6244f6792ccb2bc3b664
Prajwal122003 <83130088+Prajwal122003@users.noreply.github.com>
2022-12-11 13:19:13 +0530

    added new.py

---
+++ new.py
@@ -0,0 +1,9 @@
+#christamas
+for n in range(1,5):
+    print(" "*(8-n)+"* "*n)
+for c in range(3,7):
+    print(" "*(8-c)+"* "*c)
+for b in range(5,9):
+    print(" "*(8-b)+"* "*b)
+for a in range(1,5):
+    print(" "*4+"* "*4)
